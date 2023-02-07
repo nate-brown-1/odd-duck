b871c53e866a0a2935475c06c4e591f5384db21d
Nate Brown <nathan.d.brown@gmail.com>
2023-02-07 13:56:49 -0800

    revert commit to main, should have gone to branch This reverts commit 6e8ea589847a2cfdcbc343b850bd76ad14128762.

--- app.js
+++ app.js
@@ -4,27 +4,26 @@
 
 // declare some variables
 
-// let gridContainer = document.getElementById('grid-container');
+let gridContainer = document.getElementById('grid-container');
 
-let clickBox = document.getElementById('click-box');
-let resultsButton = document.getElementById('results-button');
-
-let roundsText = document.getElementById('rounds');
-let instructionsText = document.getElementById('instructions');
+let clickBoxes = document.getElementById('click-box');
 
 let image1 = document.getElementById('image1');
 let image2 = document.getElementById('image2');
 let image3 = document.getElementById('image3');
 
-let roundsComplete = 0;
-let roundsAllowed = 15;
+const ARRAY_OF_PRODUCTS = [];
 
+// constructor function to generate Product objects
+// name, picture, views counter, likes counter
+// push to array of products
 
-function Product(name, fileExtension = 'jpg') {
+function Product (name, fileExtension = 'jpg') {
   this.name = name;
   this.src = `img/${name}.${fileExtension}`;
   this.views = 0;
   this.likes = 0;
+  ARRAY_OF_PRODUCTS.push(this);
 }
 
 let bag = new Product('bag');
@@ -47,28 +46,6 @@ let unicorn = new Product('unicorn');
 let waterCan = new Product('water-can');
 let wineGlass = new Product('wine-glass');
 
-let ARRAY_OF_PRODUCTS = [
-  bag,
-  banana,
-  bathroom,
-  boots,
-  breakfast,
-  bubblegum,
-  chair,
-  cthulhu,
-  dogDuck,
-  dragon,
-  pen,
-  petSweep,
-  scissors,
-  shark,
-  sweep,
-  tauntaun,
-  unicorn,
-  waterCan,
-  wineGlass
-];
-
 console.log(ARRAY_OF_PRODUCTS);
 
 // generate random number for array index
@@ -77,85 +54,37 @@ function selectRandomProduct() {
 }
 
 // empty array to hold products currently shown on screen
+const DISPLAYED_IMAGE_ARRAY = [];
 
-function renderProducts() {
-  let DISPLAYED_IMAGE_ARRAY = [];
+// generate 1st random product
+// add it to the array
+let leftProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
+DISPLAYED_IMAGE_ARRAY.push(leftProduct);
 
-  // generate 1st random product
-  // add it to the array
-  let leftProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
-  DISPLAYED_IMAGE_ARRAY.push(leftProduct);
-
-  // generate 2nd random product
-  // check if it matches a product already in the array
-  // if it does, generate a new product
-  // if not, add it to the array
-  let middleProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
-  while (DISPLAYED_IMAGE_ARRAY.includes(middleProduct)) {
-    middleProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
-  }
-  DISPLAYED_IMAGE_ARRAY.push(middleProduct);
-
-  // generate 3rd random product
-  // check if it matches a product already in the array
-  // if it does, generate a new product
-  // if not, add it to the array
-  let rightProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
-  while (DISPLAYED_IMAGE_ARRAY.includes(rightProduct)) {
-    rightProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
-  }
-  DISPLAYED_IMAGE_ARRAY.push(rightProduct);
-
-  console.log(DISPLAYED_IMAGE_ARRAY);
-
-  // display all 3 products from the array
-  image1.src = `${leftProduct.src}`;
-  image1.alt = `${leftProduct.name}`;
-  leftProduct.views++;
-  console.log(leftProduct.views);
-  image2.src = `${middleProduct.src}`;
-  image2.alt = `${middleProduct.name}`;
-  middleProduct.views++;
-  console.log(middleProduct.views);
-  image3.src = `${rightProduct.src}`;
-  image3.alt = `${rightProduct.name}`;
-  rightProduct.views++;
-  console.log(rightProduct.views);
-  roundsComplete++;
-  console.log(roundsComplete);
+// generate 2nd random product
+// check if it matches a product already in the array
+// if it does, generate a new product
+// if not, add it to the array
+let middleProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
+while (DISPLAYED_IMAGE_ARRAY.includes(middleProduct)) {
+  middleProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
 }
+DISPLAYED_IMAGE_ARRAY.push(middleProduct);
 
-// invoke function to display 1st set of products on initial page load
-renderProducts();
-
-// event handler for clicks
-function handleProductClick(event) {
-  console.log(event.target);
-  console.log(event.target.alt);
-  let productClicked = event.target.alt;
-  for (let i = 0; i < ARRAY_OF_PRODUCTS.length; i++) {
-    if (ARRAY_OF_PRODUCTS[i].name === productClicked) {
-      ARRAY_OF_PRODUCTS[i].likes++;
-    }
-  }
-  if (roundsComplete < roundsAllowed) {
-    renderProducts();
-  } else {
-    clickBox.removeEventListener('click', handleProductClick);
-    instructionsText.textContent = ('Rounds complete! Click the button to the left to see the results.');
-    roundsText.remove();
-    resultsButton.addEventListener('click', renderResults);
-  }
+// generate 3rd random product
+// check if it matches a product already in the array
+// if it does, generate a new product
+// if not, add it to the array
+let rightProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
+while (DISPLAYED_IMAGE_ARRAY.includes(rightProduct)) {
+  rightProduct = ARRAY_OF_PRODUCTS[selectRandomProduct()];
 }
+DISPLAYED_IMAGE_ARRAY.push(rightProduct);
 
-clickBox.addEventListener('click', handleProductClick);
+console.log(DISPLAYED_IMAGE_ARRAY);
 
-function renderResults() {
-  let results = document.querySelector('ul');
-  for (let i = 0; i < ARRAY_OF_PRODUCTS.length; i++) {
-    let li = document.createElement('li');
-    li.textContent = `${ARRAY_OF_PRODUCTS[i].name} was viewed ${ARRAY_OF_PRODUCTS[i].views} times and received ${ARRAY_OF_PRODUCTS[i].likes} likes`;
-    results.appendChild(li);
-  }
-  resultsButton.removeEventListener('click', renderResults);
-}
+// display all 3 products from the array
+image1.src = `${DISPLAYED_IMAGE_ARRAY[0].src}`;
+image2.src = `${DISPLAYED_IMAGE_ARRAY[1].src}`;
+image3.src = `${DISPLAYED_IMAGE_ARRAY[2].src}`;
+
